8b88ca6df7638decaa1a5ad769a18085e8c324f8
YYZiMig <a1738834479@gmail.com>
2021-09-11 00:42:00 +0800

    initial project

---
+++ components/MessageItem.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="massage-item">
+		<view class="picture">
+			头像
+		</view>
+		<view class="content">
+			<view class="title">
+				<text>
+					{{title}}
+				</text>
+				<text class="date">
+					{{date}}
+				</text>
+			</view>
+			<view class="message">{{message}}</view>
+		</view>
+	</view>
+</template>
+
+<script lang="ts">
+	import {Component,Vue,Prop} from "vue-property-decorator";
+	@Component
+	export default class MessageItem extends Vue {
+		public name:String="MessageItem";
+		@Prop({type:String,required:true})
+		private title:String;
+		@Prop({type:String,required:true})
+		private date:String;
+		@Prop({type:String,default:""})
+		private message:String;
+	}
+</script>
+<style lang="scss">
+	.massage-item{
+		@extend .flex-row-between-center;
+		padding: 10rpx 32rpx;
+		.content{
+			flex-grow: 1;
+			.title{
+				@extend .flex-row-between;
+				font-size: $uni-font-size-lg;
+				.date{
+					font-size: $uni-font-size-sm;
+				}
+			}
+			.message{
+				font-size: $uni-font-size-base;
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<view class="head-menu">
+			<view class="user-namepicture">
+				1
+			</view>
+			<view class="user-name">
+				{{name}}
+			</view>
+			<view class="menu">
+				<view class="search"></view>
+				<view class="more">+</view> 
+			</view>
+		</view>
+		<view class="all-message">
+			<MessageItem :title="name" :message="name" date="127.0.1"></MessageItem>
+		</view>
+	</view>
+</template>
+
+<script lang="ts">
+	import MessageItem from "@/components/MessageItem.vue";
+	import {Component,Vue,Prop} from "vue-property-decorator";
+	@Component({components: {MessageItem }})
+	export default class Index extends Vue {
+		private name:String = "YYZiMig";
+		onLoad() {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		@extend .flex-col;
+		background-color: $uni-color-conent-bg;
+		$top-height:88rpx;
+		.head-menu{
+			height: $top-height;
+			background:$uni-color-conent-white;
+			padding:0 32rpx;
+			@include fixed(0,0,initial,0); 
+			@extend .bottom-line;
+			@extend .flex-row-between-center;
+		}
+		.all-message{
+			@include absolute($top-height,0,0,0);
+		}
+	}
+</style>
